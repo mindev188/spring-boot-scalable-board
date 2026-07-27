0fbb622f344504543d125b0965eea414febde50d
minimum1227.kim <minimum1227.kim@crinity.com>
2026-07-27 16:59:06 +0900

    feat: 19ch. 게시글 목록 API - 무한 스크롤 구현 (1)

--- service/article/src/main/java/mindev188/board/article/controller/ArticleController.java
+++ service/article/src/main/java/mindev188/board/article/controller/ArticleController.java
@@ -8,6 +8,8 @@ import mindev188.board.article.service.response.ArticlePageResponse;
 import mindev188.board.article.service.response.ArticleResponse;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class ArticleController {
@@ -21,6 +23,13 @@ public class ArticleController {
     @GetMapping("/v1/articles")
     public ArticlePageResponse readAll(@RequestParam("boardId") Long boardId, @RequestParam Long page, @RequestParam Long pageSize) {
        return articleService.readAll(boardId, page, pageSize);
+    }
+
+    @GetMapping("/v1/articles/infinite-scroll")
+    public List<ArticleResponse> readAllInfiniteScroll(@RequestParam("boardId") Long boardId,
+                                                       @RequestParam("pageSize") Long pageSize,
+                                                       @RequestParam(value = "lastArticleId", required = false) Long lastArticleId) {
+        return articleService.readAllInfiniteScroll(boardId, pageSize, lastArticleId);
     }
 
     @PostMapping("/v1/articles")
--- service/article/src/main/java/mindev188/board/article/service/ArticleService.java
+++ service/article/src/main/java/mindev188/board/article/service/ArticleService.java
@@ -11,6 +11,8 @@ import mindev188.board.article.service.response.ArticlePageResponse;
 import mindev188.board.article.service.response.ArticleResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
@@ -51,5 +53,12 @@ public class ArticleService {
                         PageLimitCalculator.calculatePageLimit(page, pageSize, 10L)
                 )
         );
+    }
+
+    public List<ArticleResponse> readAllInfiniteScroll(Long boardId, Long pageSize, Long lastArticleId) {
+        List<Article> articles = lastArticleId == null ?
+                articleRepository.findAllInfiniteScroll(boardId, pageSize) :
+                articleRepository.findAllInfiniteScroll(boardId, pageSize, lastArticleId);
+        return articles.stream().map(ArticleResponse::from).toList();
     }
 }
