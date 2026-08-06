6f6746ef8d69eefe5c3829150d0f0ffe87480ffb
minimum1227.kim <minimum1227.kim@crinity.com>
2026-08-06 16:57:26 +0900

    feat: 44ch. 인기글 - Event & 직렬화 모듈 구현 - 직렬화, 반직렬화 Test 코드 작성

---
+++ common/event/src/main/java/kuke/board/common/event/EventType.java
@@ -0,0 +1,40 @@
+package kuke.board.common.event;
+
+import kuke.board.common.event.payload.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@RequiredArgsConstructor
+public enum EventType {
+    ARTICLE_CREATED(ArticleCreatedEventPayload.class, Topic.SCALABLE_BOARD_ARTICLE),
+    ARTICLE_UPDATED(ArticleUpdatedEventPayload.class, Topic.SCALABLE_BOARD_ARTICLE),
+    ARTICLE_DELETED(ArticleDeletedEventPayload.class, Topic.SCALABLE_BOARD_ARTICLE),
+    COMMENT_CREATED(CommentCreatedEventPayload.class, Topic.SCALABLE_BOARD_COMMENT),
+    COMMENT_DELETED(CommentDeletedEventPayload.class, Topic.SCALABLE_BOARD_COMMENT),
+    ARTICLE_LIKED(ArticleLikedEventPayload.class, Topic.SCALABLE_BOARD_LIKE),
+    ARTICLE_UNLIKED(ArticleUnlikedEventPayload.class, Topic.SCALABLE_BOARD_LIKE),
+    ARTICLE_VIEWED(ArticleViewdEventPayload.class, Topic.SCALABLE_BOARD_VIEW);
+
+    private final Class<? extends EventPayload> payloadClass;
+    private final String topic;
+
+    public static EventType from(String type) {
+        try {
+            return valueOf(type);
+        } catch(Exception e) {
+            log.error("[EventType.from] type={}", type, e);
+            return null;
+        }
+    }
+
+    public static class Topic {
+        public static final String SCALABLE_BOARD_ARTICLE = "scalable-board-article";
+        public static final String SCALABLE_BOARD_COMMENT = "scalable-board-comment";
+        public static final String SCALABLE_BOARD_LIKE = "scalable-board-like";
+        public static final String SCALABLE_BOARD_VIEW = "scalable-board-view";
+    }
+}
