f0be04720079dd99245536008064f329772fad3e
mindev188 <mindev188@gmail.com>
2026-08-06 21:19:03 +0900

    feat: 45ch. 인기글 Consumer 구현 - Redis, Kafka 설정 - 인기글 Repository 구현 - Repository Test 구현

---
+++ service/hot-article/src/main/java/mindev188/board/hotarticle/repository/HotArticleListRepository.java
@@ -0,0 +1,62 @@
+package mindev188.board.hotarticle.repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Repository;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class HotArticleListRepository {
+    private final StringRedisTemplate redisTemplate;
+
+    // hot-article::list::{yyyyMMdd}
+    private static final String KEY_FORMAT = "hot-article::list::%s";
+
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+    public void add(Long articleId, LocalDateTime time, Long score, Long limit, Duration ttl) {
+        redisTemplate.executePipelined((RedisCallback<?>) connection -> {
+            String key = generateKey(time);
+            byte[] keyBytes = serialize(key);
+            byte[] articleIdBytes = serialize(String.valueOf(articleId));
+
+            RedisConnection conn = connection;
+            conn.zSetCommands().zAdd(keyBytes, score, articleIdBytes);
+            conn.zSetCommands().zRemRange(keyBytes, 0, -limit - 1);
+            conn.keyCommands().expire(keyBytes, ttl);
+            return null;
+        });
+    }
+
+    private byte[] serialize(String value) {
+        return value.getBytes(StandardCharsets.UTF_8);
+    }
+
+    private String generateKey(LocalDateTime time) {
+        return generateKey(TIME_FORMATTER.format(time));
+    }
+
+    private String generateKey(String dateStr) {
+        return KEY_FORMAT.formatted(dateStr);
+    }
+
+    public List<Long> readAll(String dateStr) {
+        return redisTemplate.opsForZSet()
+                .reverseRangeWithScores(generateKey(dateStr), 0, -1).stream()
+                .peek(tuple -> log.info("[HotArticleListRepository.readAll] articleId={}, score={}", tuple.getValue(), tuple.getScore()))
+                .map(ZSetOperations.TypedTuple::getValue)
+                .map(Long::valueOf)
+                .toList();
+    }
+}
